affe2f5b744f9e072bafbd06c343d4a341db13d4
Jhonni10 <jhonieripianoscue@gmail.com>
2024-06-26 20:44:12 -0500

    creating page of favorite and config redux

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,16 @@
+import { SimpleWidget } from '@/components'
 import React from 'react'
+
+export const metadata = {
+ title: 'Dashboard',
+ description: 'Dashboard',
+};
 
 export default function DashboardPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      Pagina de inicio
+       <h1 className='text-3xl font-semibold mb-4'>Panel administrativo</h1>
+      <SimpleWidget/>
     </main>
   )
 }
